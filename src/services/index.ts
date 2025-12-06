0cb289553809c056ddf6af990c9c7ee3e5e7307f
Jose Luis Cusilayme Garcia <jcusilaymeg@unsa.edu.pe>
2025-12-06 16:28:29 -0500

    feat: create index service file to export all services from a single location

---
+++ src/services/index.ts
@@ -0,0 +1,10 @@
+// Exportar todos los servicios desde un solo lugar
+export { authService } from './auth.service';
+export { diarioService } from './diario.service';
+export { emocionesService } from './emociones.service';
+export { ejerciciosService } from './ejercicios.service';
+export { foroService } from './foro.service';
+export { tipsService } from './tips.service';
+export { cuestionariosService } from './cuestionarios.service';
+export { contenidoService } from './contenido.service';
+export { api } from './api';
